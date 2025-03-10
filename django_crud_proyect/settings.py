5a72d2787396485e64966d27cd0c6978fc322ff2
gab <gabcam071118.rg@gmail.com>
2025-03-10 16:09:51 -0600

    Configure environment variables and PostgreSQL database connection

--- django_crud_proyect/settings.py
+++ django_crud_proyect/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url 
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +86,7 @@ WSGI_APPLICATION = 'django_crud_proyect.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default= os.getenv('DATABASE_URL'))
 }
 
 
@@ -146,3 +148,6 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+
+CSRF_TRUSTED_ORIGINS = ['http://', 'https://web-production-0e2a.up.railway.app']
