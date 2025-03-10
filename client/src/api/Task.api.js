7919a9ec720f7b9c3a3130636ccbf63785ad4b6e
gab <gabcam071118.rg@gmail.com>
2025-03-10 16:53:04 -0600

    Update base URL for TaskApi to production Railway endpoint

--- client/src/api/Task.api.js
+++ client/src/api/Task.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const TaskApi = axios.create({
-    baseURL: 'http://localhost:8000/task/api/v1/task/'
+    baseURL: 'https://web-production-0e2a.up.railway.app/task/api/v1/task/'
 })
 
 export const getAllTask = () => TaskApi.get('/')
